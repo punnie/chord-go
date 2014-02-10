59eb804a5f929b2f9f0189b2b362e2762b5b36cb
Pedro Coelho <pedro.luis.coelho@gmail.com>
2014-02-10 19:39:42 +0000

    Implemented some messaging exchange mechanisms into Node

--- node.go
+++ node.go
@@ -6,13 +6,14 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
-  "time"
+	"time"
 )
 
 type Node struct {
 	id           *KeyID
 	address      string
 	conn         net.Conn
+  writer       *bufio.Writer
 	localInbound chan *Message
 }
 
@@ -29,14 +30,23 @@ func NewNode(address string) *Node {
 	n := &Node{
 		id:      i,
 		address: address,
+    localInbound: make(chan *Message),
 	}
 
 	fmt.Println("new node id hex:", i.String())
 	return n
 }
 
+func FakeNode(hex string) *Node {
+  i := NewKeyID(hex)
+
+  return &Node{
+    id: i,
+  }
+}
+
 func (n *Node) Accept(conn net.Conn, globalInbound chan<- *Message) {
-  n.conn = conn
+	n.conn = conn
 
 	go n.handleConnection(globalInbound)
 }
@@ -89,12 +99,11 @@ func (n *Node) handleConnection(globalInbound chan<- *Message) {
 		case REPLY_SUCCESSOR:
 			fallthrough
 		case REPLY_PING:
-      println(mes.String())
 			n.localInbound <- mes
 		case REQUEST_SUCCESSOR:
 			fallthrough
 		case REQUEST_PING:
-      mes.Sender = n
+			mes.Sender = n
 			globalInbound <- mes
 		}
 	}
@@ -102,7 +111,12 @@ func (n *Node) handleConnection(globalInbound chan<- *Message) {
 
 func (n *Node) sendMessage(m *Message) (int, error) {
 	// TODO: verify we have an active connection
-	writer := bufio.NewWriter(n.conn)
+  if n.writer == nil {
+    n.writer = bufio.NewWriter(n.conn) // this is not threadsafe!
+  }
+
+  println("sending        :", m.String())
+
 	buf := new(bytes.Buffer)
 	payload, err := m.MessageEncode()
 
@@ -124,39 +138,65 @@ func (n *Node) sendMessage(m *Message) (int, error) {
 		return 0, err
 	}
 
-	_, err = buf.WriteTo(writer)
+	_, err = buf.WriteTo(n.writer)
 
 	if err != nil {
 		return 0, err
 	}
 
-	writer.Flush()
+	n.writer.Flush()
+
 	return w, nil
 }
 
-func (n *Node) GetSuccessor(node *Node) (*Node, error) {
+func (n *Node) RequestSuccessor(node *Node) (*Node, error) {
 	queryKey := node.Id()
 	n.sendMessage(NewFindSuccessorMessage([]string{queryKey.String()}))
 
-  reply := <-n.localInbound
-
-  println("received reply:", reply)
+	reply := <-n.localInbound
+	println("received reply :", reply.String())
 
 	return &Node{}, nil
 }
 
-func (n *Node) GetPredecessor() (*Node, error) {
+func (n *Node) ReplySuccessor(node *Node) error {
+  reply := &Message{
+    Intent: REPLY_SUCCESSOR,
+    Parameters: []string{node.Id().String(), node.Address()},
+    Timestamp: time.Now().UTC(),
+  }
+
+  w, err := n.sendMessage(reply)
+
+  println("sending size   :", w)
+
+  if err != nil {
+    return err
+  }
+
+  return nil
+}
+
+func (n *Node) RequestPredecessor() (*Node, error) {
 	return nil, nil
 }
 
 func (n *Node) SendPing() error {
-  n.sendMessage(&Message{Intent: REQUEST_PING, Timestamp: time.Now().UTC()})
+  _, err := n.sendMessage(&Message{Intent: REQUEST_PING, Timestamp: time.Now().UTC()})
 
-  return nil
+  if err != nil {
+    return err
+  }
+
+	return nil
 }
 
 func (n *Node) ReplyPing() error {
-  n.sendMessage(&Message{Intent: REPLY_PING, Timestamp: time.Now().UTC()})
+  _, err := n.sendMessage(&Message{Intent: REPLY_PING, Timestamp: time.Now().UTC()})
 
-  return nil
+  if err != nil {
+    return err
+  }
+
+	return nil
 }
